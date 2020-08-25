3f81dd463aad60ce28ba673fd98ecca3fe74ae40
Luiz Fernando <devfernandoof@gmail.com>
2020-08-24 23:59:49 -0300

    🚧  API get all data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 import React from "react";
+import Tmdb from "./Tmdb";
 
-function App() {}
+function App() {
+  const [movieList, setMovieList] = React.useState([]);
+
+  React.useEffect(() => {
+    const loadAll = async () => {
+      // GET THE TOTAL LIST
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+    };
+
+    loadAll();
+  }, []);
+
+  return (
+    // HEADER
+    // Trending
+    // Lists
+    // Footer
+    <div></div>
+  );
+}
 
 export default App;
--- src/Tmdb.js
+++ src/Tmdb.js
@@ -1,5 +1,12 @@
 const API_KEY = "a91c85266bb9abfe3c0023fb750c21ac";
-const API_BASE = "https://api.themoviedb.org/3/";
+const API_BASE = "https://api.themoviedb.org/3";
+
+const basicFetch = async (endpoint) => {
+  const req = await fetch(`${API_BASE}${endpoint}`);
+  const json = await req.json();
+  console.log(json);
+  return json;
+};
 
 export default {
   getHomeList: async () => {
@@ -7,37 +14,54 @@ export default {
       {
         slug: "originals",
         title: "Netflix originals",
-        items: [],
+        items: await basicFetch(
+          `/discover/tv?with_network=213&api_key=${API_KEY}`
+        ),
       },
       {
         slug: "trending",
         title: "Recomendeds for your",
-        items: [],
+        items: await basicFetch(`/trending/all/week?api_key=${API_KEY}`),
+      },
+      {
+        slug: "toprated",
+        title: "Top rated",
+        items: await basicFetch(`/movie/top_rated?api_key=${API_KEY}`),
       },
       {
         slug: "action",
         title: "Action",
-        items: [],
+        items: await basicFetch(
+          `/discover/movie?with_genres=28&api_key=${API_KEY}`
+        ),
       },
       {
         slug: "comedy",
         title: "Comedy",
-        items: [],
+        items: await basicFetch(
+          `/discover/movie?with_genres=35&api_key=${API_KEY}`
+        ),
       },
       {
         slug: "horror",
         title: "Horror",
-        items: [],
+        items: await basicFetch(
+          `/discover/movie?with_genres=27&api_key=${API_KEY}`
+        ),
       },
       {
         slug: "romance",
         title: "Romance",
-        items: [],
+        items: await basicFetch(
+          `/discover/movie?with_genres=10749&api_key=${API_KEY}`
+        ),
       },
       {
         slug: "documentary",
         title: "Documentary",
-        items: [],
+        items: await basicFetch(
+          `/discover/movie?with_genres=99&api_key=${API_KEY}`
+        ),
       },
     ];
   },
