792142499a1a41819294060b36dde79b8d296100
Luiz Fernando <devfernandoof@gmail.com>
2020-08-25 01:19:14 -0300

    🚧 Styles and get featured

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from "react";
+import "./App.css";
 import Tmdb from "./Tmdb";
+import MovieRow from "./components/MovieRow";
+import FeaturedMovie from "./components/FeatureMovie";
 
 function App() {
   const [movieList, setMovieList] = React.useState([]);
+  const [featureData, setFeatureData] = React.useState(null);
 
   React.useEffect(() => {
     const loadAll = async () => {
       // GET THE TOTAL LIST
       let list = await Tmdb.getHomeList();
       setMovieList(list);
+      // GET FEATURED
+      let originals = list.filter((i) => i.slug === "originals");
+      let randomChosen = Math.floor(
+        Math.random() * (originals[0].items.results.length - 1)
+      );
+      let chosen = originals[0].items.results[randomChosen];
     };
 
     loadAll();
@@ -19,7 +29,14 @@ function App() {
     // Trending
     // Lists
     // Footer
-    <div></div>
+    <div className="page">
+      {featureData && <FeaturedMovie item={featureData} />}
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
+    </div>
   );
 }
 
--- src/Tmdb.js
+++ src/Tmdb.js
@@ -4,7 +4,6 @@ const API_BASE = "https://api.themoviedb.org/3";
 const basicFetch = async (endpoint) => {
   const req = await fetch(`${API_BASE}${endpoint}`);
   const json = await req.json();
-  console.log(json);
   return json;
 };
 
